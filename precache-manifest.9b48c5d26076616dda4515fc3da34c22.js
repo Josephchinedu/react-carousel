51606af6148fb6dbac1d7e60fd0f1f6a2bee0c26
Josephchinedu <joseph@digitaldesign247.com>
2020-04-07 01:46:18 +0100

    first commit

---
+++ precache-manifest.9b48c5d26076616dda4515fc3da34c22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ece728f2bc2ba48420e2d526d8d6109",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1f3bd84863684502db7",
+    "url": "/static/css/2.183aa86f.chunk.css"
+  },
+  {
+    "revision": "c205481efd03f5dea2c3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e1f3bd84863684502db7",
+    "url": "/static/js/2.3c207ae7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3c207ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c205481efd03f5dea2c3",
+    "url": "/static/js/main.59890216.chunk.js"
+  },
+  {
+    "revision": "e3063e6483a16e1add3a",
+    "url": "/static/js/runtime-main.5b0a12fd.js"
+  }
+]);
